eb2e491bbdad1e7ab44eb37407d69ba337266092
Yugal Dave <iyugaldave@gmail.com>
2021-11-23 19:13:15 +0530

    for add while loop

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,11 +3,26 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
-
-// Using `console.log` log all the evan values from 1 to 10.
-
-// Using `console.log` log all the od values from 1 to 10.
-
+for(let i = 0; i < 10; i = i + 1){
+    console.log(i);
+}
+// Using `console.log` log all the even values from 1 to 10.
+for(let i = 0; i < 10; i = i + 1){
+    if(i % 2 == 0)
+    console.log(i)
+}
+// Using `console.log` log all the odd values from 1 to 10.
+for(let i = 0; i < 10; i = i + 1){
+    if(i % 2 == 1)
+    console.log(i)
+}
 // Calculate the sum of all numbers from 1 to 10.
-
+for(let s = 0; s < 10; s = s+1){
+    console.log(s)
+}
 // Log all the values from 1 to 10 using while loop
+let v = 0
+while(v < 10){
+    console.log(v)
+    v = v + 1;
+}
