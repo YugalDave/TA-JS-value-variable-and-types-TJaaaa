8a1f48e90dda694e388e01d84d05585dd9e03571
Yugal Dave <iyugaldave@gmail.com>
2021-12-01 19:45:02 +0530

    adding

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -11,13 +11,21 @@ let lastName = "Stark";
 // Assign a new value of `"Snow"` to lastName.
 // Alert the new value of `firstName` and `lastName` by concatenating them
 // Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
-
+alert(firstName);
+alert(lastName);
+firstName = `Jhon`;
+lastName = `Snow`;
+alert(firstName + ` ` + lastName);
+const fullname = firstName + ` ` + lastName;
 // Declare three variables `name,` `newUser` and `isAdmin`.
 // Assign your name as the value of `name`.
 // Assign the value of `name` to `newUser`.
 // Set the value of `isAdmin` to `true`.
 // Alert the values of all the three variables.
-
+var name = `yugal`;
+var newUser = name;
+var isAdmin = true;
+alert(name + ` ` + newUser + ` ` + isAdmin);
 // Assignment and re-assignment
 
 var wiseMan = "Tyrion Lannister";
@@ -28,7 +36,10 @@ var wiseMan = "Tyrion Lannister";
 // Declare a variable `userName` with value "Lysa Arryn"
 // Declare a variable named `oddNumber` and assign a value `57`.
 // Reassign the value of `oddNumber` to 61
-
+wiseMan = `Samwell Tarly`;
+var userName = `Lyra Arryan`;
+var oddNumber = 57;
+oddNumber = 61;
 // Mathematical Operation
 
 let amount = 4280;
@@ -39,3 +50,8 @@ let amount = 4280;
 // Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
 // Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
 // Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
+
+var reducedAmount = alert(amount - 24);
+var addedAmount = alert(amount + 32);
+var multipleAmount = alert(amount * 7);
+var dividedAmount = alert(amount / 57);
